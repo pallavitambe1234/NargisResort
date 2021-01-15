44d1027f485c04330732fbbcdba4a4fea16c496d
Vaishnavi Baporikar <vaishnavibaporikar10@gmail.com>
2021-01-15 18:04:21 +0530

    Package
    done

--- package.php
+++ package.php
@@ -25,24 +25,24 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-6 col-md-6 col-sm-12">
-					<img src="images/event/trip.jpg" alt="" class="img-fluid">
+					<img src="images/event/11.jpeg" alt="" class="img-fluid">
 				</div>
 				<div class="col-lg-6 col-md-6 col-sm-12 text-center">
 					<div class="inner-column">
-						<h1><span>Family Package</span></h1>
+						<h1><span>Amenities</span></h1>
 						
-						<p>Super Deluxe Tree House : Per Family(2+2) RS.5999(Per Day)
+						<!--<p>Super Deluxe Tree House : Per Family(2+2) RS.5999(Per Day)
  </p>
-						<p>Deluxe Tree House       : Per Family(2+2) RS.4999(Per Day)</p>
+						<p>Deluxe Tree House       : Per Family(2+2) RS.4999(Per Day)</p>-->
 						
 					</div>
 				</div>
 				
 				<div class="col-lg-6 col-md-6 col-sm-12 text-center">
 					<div class="inner-column">
-						<h1><span>Group Package</span></h1>
+						<h1><span>Family Packages</span></h1>
 						
-						<p>1.10:00AM to 05:00PM Per Person RS.799
+						<!--<p>1.10:00AM to 05:00PM Per Person RS.799
  </p>
 						<p>2.10:00AM to 10:00PM Per Person RS.1199
 </p>
@@ -59,34 +59,34 @@
 </p>
 <p>5.Dormitory:RS.999 Per Person For 24Hrs
 
-</p>
+</p>-->
 						
 					</div>
 				</div>
 				<div class="col-lg-6 col-md-6 col-sm-12">
-					<img src="images/room3.jpg" alt="" class="img-fluid">
+					<img src="images/event/Whispering Teaks Package1.png" alt="" class="img-fluid">
 				</div>
 				<div class="col-lg-6 col-md-6 col-sm-12">
-					<img src="images/child2.jpg" alt="" class="img-fluid">
+					<img src="images/event/Special Attractions.png" alt="" class="img-fluid">
 				</div>
 				<div class="col-lg-6 col-md-6 col-sm-12 text-center">
 					<div class="inner-column">
-						<h1><span>Children Package</span></h1>
+						<h1><span>Special Attractions</span></h1>
 						
-						<p>Age 01-05Years Per child 300RS
+						<!--<p>Age 01-05Years Per child 300RS
  </p>
 						<p>Age 06-12Years Per child 50% of adults 
 </p>
 <p>Above 12Years Full Charges.
 
 
-</p>
+</p>-->
 
 						
 					</div>
 				</div>
 				
-				<div class="col-lg-6 col-md-6 col-sm-12 text-center">
+				<!--<div class="col-lg-6 col-md-6 col-sm-12 text-center">
 					<div class="inner-column">
 						<h1><span>Dinner Package</span></h1>
 						<h3><span>RS.399 Per person Includes</span></h3>
@@ -110,14 +110,14 @@
 
 						
 					</div>
-				</div>
-				<div class="col-lg-6 col-md-6 col-sm-12">
+				</div>-->
+				<!--<div class="col-lg-6 col-md-6 col-sm-12">
 					<img src="images/cam.jpg" alt="" class="img-fluid">
-				</div>
-				<div class="col-lg-6 col-md-6 col-sm-12">
+				</div>-->
+				<!--<div class="col-lg-6 col-md-6 col-sm-12">
 					<img src="images/boat1.jpg" alt="" class="img-fluid">
-				</div>
-				<div class="col-lg-6 col-md-6 col-sm-12 text-center">
+				</div>-->
+				<!--<div class="col-lg-6 col-md-6 col-sm-12 text-center">
 					<div class="inner-column">
 						<h1><span>Lunch Package</span></h1>
 						<h3><span>RS.399 Per person Includes</span></h3>
@@ -139,7 +139,7 @@
 
 						<a class="btn btn-lg btn-circle btn-outline-new-white" href="contact.php">Reservation</a>
 					</div>
-				</div>
+				</div>-->
 			</div>
 		</div>
 	</div>
