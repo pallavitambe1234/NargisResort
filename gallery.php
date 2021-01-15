e833f81c7a1682a6e5e8b5a26b325a9ea99c5614
pallavitambe1234 <56679787+pallavitambe1234@users.noreply.github.com>
2021-01-15 17:21:25 +0530

    16/1/2021
    gallary update

--- gallery.php
+++ gallery.php
@@ -25,13 +25,18 @@
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="heading-title text-center">
-						<h2>Gallery</h2>
+						
 						<!-- <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p> -->
 					</div>
 				</div>
 			</div>
 			<div class="tz-gallery">
 				<div class="row">
+										<div class="col-sm-12 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/welcome.jpg">
+							<img class="img-fluid" src="images/welcome.jpg" alt="Gallery Images">
+						</a>
+					</div>
 					<div class="col-sm-12 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/room3.jpg">
 							<img class="img-fluid" src="images/room3.jpg" alt="Gallery Images">
@@ -77,6 +82,64 @@
 							<img class="img-fluid" src="images/diner2.jpg" alt="Gallery Images">
 						</a>
 					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/cam.jpg">
+							<img class="img-fluid" src="images/cam.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/boat1.jpg">
+							<img class="img-fluid" src="images/boat1.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/child2.jpg">
+							<img class="img-fluid" src="images/child2.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/rest2.jpg">
+							<img class="img-fluid" src="images/rest2.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/rest.jpg">
+							<img class="img-fluid" src="images/rest.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/IMG-20201204-WA0016.jpg">
+							<img class="img-fluid" src="images/IMG-20201204-WA0016.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/ro.jpg">
+							<img class="img-fluid" src="images/ro.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/pq.jpg">
+							<img class="img-fluid" src="images/pq.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/IMG-20201204-WA0007.jpg">
+							<img class="img-fluid" src="images/IMG-20201204-WA0007.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/IMG-20201204-WA0008.jpg">
+							<img class="img-fluid" src="images/IMG-20201204-WA0008.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/rrr.jpeg">
+							<img class="img-fluid" src="images/rrr.jpeg" alt="Gallery Images">
+						</a>
+					</div>
+					
+					
 				</div>
 			</div>
 		</div>
